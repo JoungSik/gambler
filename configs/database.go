db9255f192f957715250ca1fb3671bb799e1d632
JoungSik <tjstlr2010@gmail.com>
2022-01-15 17:45:28 +0900

    #1 주석 제거

--- configs/database.go
+++ configs/database.go
@@ -9,7 +9,6 @@ import (
 func InitDB(debug bool) (*gorm.DB, error) {
 	config := NewDBConfig(true)
 
-	// dsn := "host=" + config.DB_HOST + " user=" + config.DB_USER + " password=" + config.DB_PASSWORD + " dbname=gambler port=5432 sslmode=disable"
 	dsn := config.DB_USER + ":" + config.DB_PASSWORD + "@tcp(" + config.DB_HOST + ":3306)/gambler?charset=utf8mb4"
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
